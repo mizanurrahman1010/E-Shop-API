98d0720c06fe95365983eb3df53803142b6800bf
Md. Mizanur Rahman <79052925+mizanurrahmanreyad@users.noreply.github.com>
2021-07-12 23:22:28 +0600

    Updated with image upload.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('register',[UserController::class,'register']);
 Route::post('login',[UserController::class,'login']);
+Route::post('addproduct',[ProductController::class,'addProduct']);
