7c4706395bde316a879394c18c9dee5b76a4c986
Md. Mizanur Rahman <79052925+mizanurrahmanreyad@users.noreply.github.com>
2021-07-13 23:52:29 +0600

    Add product Feature

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     function addProduct(Request $req)
     {
-        return $req->file('file')->store('products');
+        $product= new Product;
+        $product->name=$req->input('name');
+        $product->price=$req->input('price');
+        $product->description=$req->input('description');
+        $product->file_path=$req->file('file')->store('products');
+        $product->save();
+
+        return $product;
     }
 }
